70953ca71de86a99ce142a0141782e6b334faff4
SangeetaJena <139542837+SangeetaJena@users.noreply.github.com>
2024-01-11 13:56:46 +0530

    Add files via upload

---
+++ PythonBasics/ConvertKilotomiles.py
@@ -0,0 +1,8 @@
+# Function for converting kilometers into miles
+def convertKilometre(km):  
+    conversion_ratio= 0.621371  
+    miles = km * conversion_ratio  
+    print ("The speed in Miles: ", miles)  
+    
+km = float (input ("Enter the speed in Kilometre: "))  
+convertKilometre(km)
---
+++ PythonBasics/sumoftwo.py
@@ -0,0 +1,7 @@
+# Python program to add two numbers
+num1 = 18
+num2 = 7
+# Adding two numbers
+sum = num1 + num2
+# printing values
+print("Sum of {0} and {1} is {2}".format(num1,num2,sum))
